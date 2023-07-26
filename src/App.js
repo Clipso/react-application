d4de2cb3c95887e5ffe0ccb7be8bd6bfca2f1abd
murtada <clipso35@gmail.com>
2023-07-26 21:14:35 +0800

    fetching api, add search bar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Employees from './pages/Employees';
 import Customers from './pages/Customers';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dictionary from './pages/Dictionary';
+import Definition from './pages/Definition';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
               <Routes>
                 <Route path="/employees" element={<Employees />} />
                 <Route path="/dictionary" element={<Dictionary />} />
+                <Route path="/definition/:search" element={<Definition />} />
                 <Route path="/customers" element={<Customers />} />
               </Routes>
             </Header>
